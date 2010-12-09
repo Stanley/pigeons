45fdca41cd470ca3337ec9b13af91ac5ed80a326
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2010-12-09 13:03:47 +0100

    + take css selectors as an option + config file with csses for 2 extra citys (wa + sk)

--- import.js
+++ import.js
@@ -8,56 +8,147 @@ var sys = require('sys')
     couchdb = require('couchdb'),
     sizzle = require('./node-sizzle'),
     Iconv  = require('iconv').Iconv,
-    iconv = new Iconv('ISO-8859-2', 'UTF-8'),
+    sprintf = require('sprintf').sprintf,
     std = process.stdout;
 
-var db = couchdb.createClient(5984, 'localhost').db('krapi-tmp'),
-    base_url = 'rozklady.mpk.krakow.pl',
-    mpk = http.createClient(80, base_url);
+var Client = exports.Client = function(options){
+  this.db = couchdb.createClient(5984, 'localhost').db(options.code);
+  this.base_url = options.server;
+  this.uri = options.uri;
+  this.mpk = http.createClient(80, this.base_url);
+  this.opts = options.get;
+  this.iconv = new Iconv(options.encoding, 'UTF-8');
+}
 
-var getTimetable = function(uri, link, cb) {
+Client.prototype.getAll = function(){
+
+  var self = this;
+  var opts = this.opts;
+
+  // Skanuj stronę główną i zapisz linki linii
+  request({ uri: 'http://'+ this.base_url + this.uri }, function(err, resp, body){
+    var window = jsdom.jsdom(body).createWindow(),
+        $ = sizzle.loadSizzle(window.document);
+
+    var lines = $(opts.lines[0]) //$('a[href$=htm]')
+    var cb = function(){
+      if(lines[0]){
+        var href = lines.shift().href
+        href = href.replace.apply(href, opts.lines[1])
+        self.getLines(href, cb, true)
+      }
+    }
+    cb()
+  })
+}
+
+Client.prototype.getLines = function(uri, cb, is_first){
+
+  var self = this;
+  var opts = this.opts;
+  var url;
+
+  if(uri.slice(0,7) == 'http://')
+    url = uri 
+  else
+    url = 'http://'+ this.base_url +'/'+ uri 
+
+  // var uri = link.replace('rw', 'w0');
+  // sys.puts('http://'+base_url+'/'+line)
+  // Odwiedź wszystkie linie i zapisz linki do przystanków i przeciwnych linii
+  std.write('.'); // Request line
+  request({ uri: url}, function(err, resp, body){
+
+    var window = jsdom.jsdom(body).createWindow(),
+        $ = sizzle.loadSizzle(window.document);
+
+    // Links to timetables
+    var timetables = $(opts.timetables[0]) //$('a[href][target="R"]')
+    var nextTimetable = function(){
+      if(timetables[0]){ // TODO timetables.shift().replace()
+        var href = timetables.shift().href
+        href = href.replace.apply(href, opts.timetables[1])
+        self.getTimetable(uri, href, nextTimetable)
+      } else
+        cb()
+    }
+    nextTimetable()
+
+    // It will query opposite line simultaneously
+    if(is_first && opts.opposite){
+      // Links to opposite directions (lines)
+      // Odwiedź wszystkie pozostałe liniie i zapisz linki do przystanków
+      var lines = $(opts.opposite) //$('a[target="_parent"]:not(:contains(*))') //.each(function(){
+      var base = uri.split('/');
+      var nextLine = function(){
+        if(lines[0])
+          self.getLines(base.slice(0, base.length-1).join('/') +'/'+ lines.shift().href, nextLine)
+      }
+      nextLine()
+    }
+  })
+}
+
+Client.prototype.getTimetable = function(uri, link, cb) {
+
+  var self = this;
+  var opts = this.opts;
+
+  if(link.slice(0,7) == 'http://')
+    url = link 
+  else
+    url = 'http://'+ this.base_url +'/'+ link 
 
   std.write('*'); // Request timetable
   var base = uri.split('/');
   uri = base.slice(0, base.length-1).join('/') +'/'+ link.href;
-  var mpk = http.createClient(80, base_url);
-  var timetable = mpk.request('GET', '/'+uri, { host: base_url })
+  // var mpk = http.createClient(80, base_url);
+  var timetable = this.mpk.request('GET', url, { host: this.base_url })
   timetable.end()
   timetable.on('response', function(response){
     var html = ''
     response.on('data', function(chunk){
-      html += iconv.convert(chunk)
+      html += self.iconv.convert(chunk)
     })
 
     response.on('end', function(){
+
       var window = jsdom.jsdom(html).createWindow();
       var $ = sizzle.loadSizzle(window.document);
+      var j = 0;
       var doc = {},
-          re = /\d{2}.\d{2}.\d{4}/,
-          txt = $.getText($('table[border=1] tr:last-child b:first')), // TODO workaround
+          re = RegExp(opts.expiration[1]),
+          txt = $.getText($(opts.expiration[0])), // TODO workaround
           m = re.exec(txt);
 
-      doc['url'] = 'http://' + base_url + '/' + uri;
-      doc['line'] = $.getText($('table tr:first-child table tr:first-child td:first-child font'));
-      doc['stop'] = $.getText($("table tr:first-child table tr:first-child td:first-child+td font:first-child"));
-      doc['route'] = $.getText($("table tr:first-child table tr:first-child td:first-child+td font:not(:first-child)"));
+      doc['url'] = url;
+      doc['line'] = $.getText($(opts.line_no));
+      doc['stop'] = $.getText($(opts.stop_name));
+      doc['route'] = $.getText($(opts.route));
+      // doc['destination'] = 
       doc['valid_since'] = m ? m[0] : txt;
       doc['updated_at'] = new Date();
       doc['table'] = {};
 
-      $("table[border=1] tr:first-child font").forEach(function(day,i){ // TODO workaround
-        var key = day.innerHTML;
+      $(opts.labels).forEach(function(day,i){ // TODO workaround
+        var key = day.innerHTML; //$.getText(day);
+
         doc['table'][key] = {};
-        $('table[border=1] tr:not(:first-child, :last-child) td:nth-child('+ (i*2+1) +')').forEach(function(hour){
-          var minutes = hour.nextSibling.innerHTML.split(' ');
-          minutes.shift(); // last element is always empty *cough*
+        //sys.inspect(sys.puts(key))
+        $(sprintf(opts.hours, i, i+1)).forEach(function(hour){
+          var minutes = $(sprintf(opts.minutes, j, j+1)).
+                        map(function(min){return min.innerHTML}); //hour.nextSibling.innerHTML.split(' ');
 
           if(minutes != ["-"]) // <- funny android face o_0
-          doc['table'][key][$.getText([hour])] = minutes
+            doc['table'][key][$.getText([hour])] = minutes
+
+          j += 1;
         });
       });
 
-      db.saveDoc(doc, function(er, ok) {
+      sys.puts(sys.inspect(doc))
+
+      self.db.saveDoc(doc, function(er, ok) {
         if (er) throw new Error(JSON.stringify(er));
         // sys.puts(sys.inspect(ok))
         std.write('!'); // Document saved
@@ -65,71 +156,22 @@ var getTimetable = function(uri, link, cb) {
         var rev = ok.rev;
 
         // Save headers
-        db.request({
+        self.db.request({
           method: 'PUT',
           path: '/'+ id +'/headers.json?rev='+rev,
           headers: { 'Content-Type': 'application/json' },
           data: response.headers
         }, function(err,resp,body){
           // Save source
-          db.request({
+          self.db.request({
             method: 'PUT',
             path: '/'+ id +'/source.html?rev='+resp.rev,
             headers: { 'Content-Type': 'text/html' },
             data: html.replace(/iso-8859-2/g, 'utf-8')
           }, cb);
+          throw "stop"
         });
       })
     })
   })
 }
-
-var getLines = function(link, cb, first){
-
-    var uri = link.replace('rw', 'w0');
-    // sys.puts('http://'+base_url+'/'+line)
-    // Odwiedź wszystkie linie i zapisz linki do przystanków i przeciwnych linii
-    std.write('.'); // Request line
-    request({uri: 'http://'+base_url+'/'+uri}, function(err, resp, body){
-
-      var window = jsdom.jsdom(body).createWindow();
-      var $ = sizzle.loadSizzle(window.document);
-
-      // Links to timetables
-      var timetables = $('a[href][target="R"]')
-      var nextTimetable = function(){
-        if(timetables[0])
-          getTimetable(uri, timetables.shift(), nextTimetable)
-        else
-          cb()
-      }
-      nextTimetable()
-
-      // It will query opposite line simultaneously
-      if(first){
-        // Links to opposite directions (lines)
-        // Odwiedź wszystkie pozostałe liniie i zapisz linki do przystanków
-        var lines = $('a[target="_parent"]:not(:contains(*))') //.each(function(){
-        var base = uri.split('/');
-        var nextLine = function(){
-          if(lines[0])
-            getLines(base.slice(0, base.length-1).join('/') +'/'+ lines.shift().href, nextLine)
-        }
-        nextLine()
-      }
-          
-    })
-}
-
-// Skanuj stronę główną i zapisz linki linii
-request({uri: 'http://'+base_url+'/linie.aspx'}, function(err, resp, body){
-  var window = jsdom.jsdom(body).createWindow();
-  var $ = sizzle.loadSizzle(window.document);
-
-  var lines = $('a[href$=htm]')
-  var cb = function(){
-    if(lines[0])
-      getLines(lines.shift().href, cb, true)
-  }
-  cb()
-})
