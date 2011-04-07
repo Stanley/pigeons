22610225e359fad9bced0fb4d4086cccae85e5d5
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-04-08 00:12:38 +0200

    + ETag renamed base_url to server

--- spec/EtagSpec.js
+++ spec/EtagSpec.js
@@ -33,3 +33,56 @@ describe('status code 200', function(){
     //expect(this.callback).toHaveBeenCalled() todo: catch put request
   });
 });
+
+describe('ETag', function(){
+
+  it('should be stored in timetable doc', function(){
+    var doc;
+    var db = s.createServer(6001, function(){
+      var server = s.createServer(6000, function(){
+        new Pigeons({}, function(){
+          var pigeons = this;
+          pigeons.server = 'http://localhost:6000'
+          pigeons.db = 'http://localhost:6001'
+          spyOn(pigeons, 'put').andCallThrough();
+          pigeons.getTimetable('/timetables/1', function(){
+            doc = pigeons.put.mostRecentCall.args[0];
+            db.close();
+            server.close();
+          })
+        });
+      }).once('/timetables/1', function(req, resp){
+        resp.writeHead(200, {'content-type':'application/json', 'etag': 'foo'});
+        resp.write('');
+        resp.end();
+      });
+    }).once('request', s.createResponse('OK'));
+
+    waitsFor(function(){ return doc }, 'save', 1000);
+    runs(function(){
+      expect(doc.etag).toEqual('foo');
+    })
+  });
+
+  it('should be send to remote server', function(){
+    var headers;
+    var server = s.createServer(6000, function(){
+      new Pigeons({}, function(){
+        this.server = 'http://localhost:6000';
+        this.existing = {'http://localhost:6000/timetables/1': {etag: 'foo'}};
+        this.get('/timetables/1');
+      });
+    }).once('/timetables/1', function(req, resp){
+      resp.writeHead(200, {});
+      resp.write('OK');
+      resp.end();
+      server.close();
+      headers = req.headers;
+    });
+
+    waitsFor(function(){ return headers }, 'headers', 1000);
+    runs(function(){
+      expect(headers['if-none-match']).toEqual('foo');
+    });
+  });
+});
