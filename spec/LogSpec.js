e7c549cc076d35f96e38274e3fffd4ed65c7778d
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-07-04 13:08:08 +0200

    - httpstat.us request + DRYed creating servers

--- spec/LogSpec.js
+++ spec/LogSpec.js
@@ -1,28 +1,46 @@
 describe('logger', function(){
 
-  it('should log all visited pages', function(){
-    var log, pigeons = new Pigeons({ server: 'http://httpstat.us' });
-    spyOn(pigeons, 'logger');
+  var server, db, logs;
+  beforeEach(function(){
+    server = s.createServer(6000, function(){
+      db = s.createServer(6001, function(){
+        logs = s.createServer(6002, function(){});
+      });
+    });
+    waitsFor(function(){return server && db && logs})
+  });
 
-    pigeons.get('/200');
+  afterEach(function(){
+    server.close(); db.close(); logs.close();
+    server = db = logs = undefined;
+  });
+
+  it('should log all visited pages', function(){
+    var log, pigeons = new Pigeons({ server: 'http://localhost:6000' });
+
+    spyOn(pigeons, 'logger');
+    server.once('request', s.createResponse(''))
+    pigeons.get('/');
+
     waitsFor(function(){ return log = pigeons.logger.argsForCall[0]; }, 'request', 2000);
     runs(function(){
-      expect(log[0].uri).toEqual('http://httpstat.us/200');
+      expect(log[0].uri).toEqual('http://localhost:6000/');
       expect(log[0].headers).toBeDefined();
       expect(log[0].statusCode).toEqual(200);
     })
   });
 
   it('should log saved timetables', function(){
-    var pigeons = new Pigeons({ server: 'http://httpstat.us' });
+    var pigeons = new Pigeons({ server: 'http://localhost:6000' });
 
     spyOn(pigeons, 'logger');
-    pigeons.getTimetable('/200');
+    server.once('request', s.createResponse(''))
+    pigeons.getTimetable('/');
 
     waitsFor(function(){ return pigeons.logger.mostRecentCall.args }, 'request', 2000);
     runs(function(){
       var log = pigeons.logger.mostRecentCall.args[0];
-      expect(log.uri).toEqual('http://httpstat.us/200');
+      expect(log.uri).toEqual('http://localhost:6000/');
       expect(log.type).toEqual('Timetable');
       expect(log.headers).toBeDefined();
       expect(log.statusCode).toEqual(200);
@@ -30,25 +48,23 @@ describe('logger', function(){
   });
 
   it('should log times of source and db requests', function(){
-    var log, logs
-      , db = s.createServer(6001, function(){
-      logs = s.createServer(6002, function(){
-        new Pigeons({ server: 'http://httpstat.us' }, function(){
-          this.db = 'http://user:password@localhost:6001';
-          this.log = 'http://localhost:6002';
-          this.getTimetable('/200');
-        });
-      }).once('request', function(req, resp){
-        var buffer = '';
-        req.on('data', function(chunk){ buffer += chunk });
-        req.on('end', function(){
-          resp.end();
-          db.close();
-          logs.close();
-          log = JSON.parse(buffer);
-        });
+    var log;
+
+    server.once('request', s.createResponse('')); // Return timetable
+    db.once('request', s.createResponse(JSON.stringify({ ok: true, id: 'foo', rev: '1-bar' }), 201));
+    logs.once('request', function(req, resp){
+      var buffer = '';
+      req.on('data', function(chunk){ buffer += chunk });
+      req.on('end', function(){
+        log = JSON.parse(buffer);
       });
-    }).once('request', s.createResponse(JSON.stringify({ ok: true, id: 'foo', rev: '1-bar' }), 201));
+    });
+
+    new Pigeons({ server: 'http://localhost:6000' }, function(){
+      this.db = 'http://user:password@localhost:6001';
+      this.log = 'http://localhost:6002';
+      this.getTimetable('/');
+    });
 
     waitsFor(function(){ return log }, 1000, 'log');
     runs(function(){
@@ -61,30 +77,31 @@ describe('logger', function(){
   it('should deprecate not existing timetables and log them', function(){
 
     var log, finished; //, doc = { type: 'Timetable', valid_from: '13.03.2011', source: 'http://localhost:6000/timetables/1' }
-    var db = s.createServer(6001, function(){
-      pigeons = new Pigeons({ server: 'http://httpstat.us', home: '/200', db: 'http://localhost:6001' }, function(){
-        var logs = s.createServer(6002, function(){
-          pigeons.log = 'http://localhost:6002';
-          pigeons.getAll(function(){
-            logs.close();
-            db.close();
-            finished = true;
-          });
-        }).on('request', function(req, resp){
-          var buffer = '';
-          req.on('data', function(chunk){ buffer += chunk });
-          req.on('end', function(){
-            resp.writeHead(201, {'content-type':'application/json'});
-            resp.write(JSON.stringify({ok: true}));
-            resp.end();
-            log = JSON.parse(buffer);
-          });
-        });
-      });
-    }).once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [
-        { value: { _id: 'foo', type: 'Timetable', valid_from: '13.03.2011', source: 'http://localhost:6000/timetables/1' }}
-      ]})))
+
+    server.once('request', s.createResponse(''))
+
+    db.once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [
+          { value: { _id: 'foo', type: 'Timetable', valid_from: '13.03.2011', source: 'http://localhost:6000/timetables/1' }}
+        ]})))
       .on('request', s.createResponse('ok'));
+
+    logs.on('request', function(req, resp){
+      var buffer = '';
+      req.on('data', function(chunk){ buffer += chunk });
+      req.on('end', function(){
+        resp.writeHead(201, {'content-type':'application/json'});
+        resp.write(JSON.stringify({ok: true}));
+        resp.end();
+        log = JSON.parse(buffer);
+      });
+    });
+
+    new Pigeons({ server: 'http://localhost:6000', home: '/', db: 'http://localhost:6001' }, function(){
+      this.log = 'http://localhost:6002';
+      this.getAll(function(){
+        finished = true;
+      });
+    });
 
     waitsFor(function(){ return finished }, 'log', 1000);
     runs(function(){
@@ -95,67 +112,61 @@ describe('logger', function(){
   
   it('should deprecate outdated timetables (newer avaliable)', function(){
     var callback = jasmine.createSpy();
-    var log, finished, server = s.createServer(6000, function(){
-      pigeons = new Pigeons({ server: 'http://localhost:6000', home: '/',
-        get: { lines: 'a', timetables: 'a', valid_from: '.valid_from' } },
-        function(){
-          var db = s.createServer(6001, function(){
-            var logs = s.createServer(6002, function(){
-              pigeons.db = 'http://localhost:6001';
-              pigeons.log = 'http://localhost:6002';
-              pigeons.getAll(function(){
-                server.close();
-                logs.close();
-                db.close();
-                finished = true;
-              });
-            }).on('request', function(req, resp){
-              var buffer = '';
-              req.on('data', function(chunk){ buffer += chunk });
-              req.on('end', function(){
-                resp.writeHead(201, {'content-type':'application/json'});
-                resp.write(JSON.stringify({ok: true}));
-                resp.end();
-                log = JSON.parse(buffer);
-              });
-            });
-          }).once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [{ value: 
-            { _id: 'foo', type: 'Timetable', valid_from: '13.03.2011', source: 'http://localhost:6000/timetables/1' }
-          }]})))
-            .once('/_design/Timetable/_update/dump/foo?new_doc_since=14.03.2011', callback)
-            .on('request', s.createResponse('{"ok": true}'));
-        }
-      );
-    }).once('/', s.createResponse("<html><body><div><a href=\"/lines/1\">1</a></div></body></html>"))
+    var log, finished;
+
+    logs.on('request', function(req, resp){
+      var buffer = '';
+      req.on('data', function(chunk){ buffer += chunk });
+      req.on('end', function(){
+        resp.writeHead(201, {'content-type':'application/json'});
+        resp.write(JSON.stringify({ok: true}));
+        resp.end();
+        log = JSON.parse(buffer);
+      });
+    });
+
+    db.once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [{ value: 
+          { _id: 'foo', type: 'Timetable', valid_from: '13.03.2011', source: 'http://localhost:6000/timetables/1' }
+        }]})))
+      .once('/_design/Timetable/_update/dump/foo?new_doc_since=14.03.2011', callback)
+      .on('request', s.createResponse('{"ok": true}'));
+
+    server.once('/', s.createResponse("<html><body><div><a href=\"/lines/1\">1</a></div></body></html>"))
       .once('/lines/1', s.createResponse("<html><body><div><a href=\"/timetables/1\">1</a></div></body></html>"))
       .once('/timetables/1', s.createResponse("<html><body><div class=\"valid_from\">14.03.2011</div></body></html>"));
 
+    new Pigeons({ server: 'http://localhost:6000', home: '/', get: { lines: 'a', timetables: 'a', valid_from: '.valid_from' } }, function(){
+      this.db = 'http://localhost:6001';
+      this.log = 'http://localhost:6002';
+      this.getAll(function(){
+        finished = true;
+      });
+    });
+
     waitsFor(function(){ return finished }, 'request', 1000);
     runs(function(){
-      expect(callback).toHaveBeenCalled()
+      expect(callback).toHaveBeenCalled();
     })
   });
 
   it('should log requests of timetables which do not requre update', function(){
-    var log, server;
-    var pigeon = new Pigeons({ server: 'http://localhost:6000', get: { valid_from: 'div' }}, function(){
-      var logs = s.createServer(6002, function(){
-        pigeon.log = 'http://localhost:6002';
-        server = s.createServer(6000, function(){
-          pigeon.existing = {'http://localhost:6000/timetable/1': {valid_from: '25.03.2011'}}
-          pigeon.getTimetable('/timetable/1');
-        }).once('/timetable/1', s.createResponse(CreateDocument('<div>25.03.2011</div>')))
-      }).once('/', function(req, resp){
-        var buffer = '';
-        req.on('data', function(chunk){ buffer += chunk });
-        req.on('end', function(){
-          resp.end();
-          server.close();
-          logs.close();
-          log = JSON.parse(buffer);
-        });
+    var log;
+
+    server.once('/timetable/1', s.createResponse(CreateDocument('<div>25.03.2011</div>')))
+    logs.once('/', function(req, resp){
+      var buffer = '';
+      req.on('data', function(chunk){ buffer += chunk });
+      req.on('end', function(){
+        log = JSON.parse(buffer);
       });
     });
+  
+    new Pigeons({ server: 'http://localhost:6000', get: { valid_from: 'div' }}, function(){
+      this.log = 'http://localhost:6002';
+      this.existing = {'http://localhost:6000/timetable/1': {valid_from: '25.03.2011'}}
+      this.getTimetable('/timetable/1');
+    });
+
     waitsFor(function(){ return log }, 1000, 'log');
     runs(function(){
       expect(log.uri).toEqual('http://localhost:6000/timetable/1');
--- spec/SmartUpdatesSpec.js
+++ spec/SmartUpdatesSpec.js
@@ -3,28 +3,37 @@ var Pigeons = require('pigeons').Client
 
 describe('local cache check', function(){
 
+  var server, db, logs;
+  beforeEach(function(){
+    server = s.createServer(6000, function(){
+      db = s.createServer(6001, function(){
+        logs = s.createServer(6002, function(){});
+      });
+    });
+    waitsFor(function(){return server && db && logs})
+  });
+
+  afterEach(function(){
+    server.close(); db.close(); logs.close();
+    server = db = logs = undefined;
+  });
+
   it('should retrieve all recent timetables', function(){
 
     var pigeons, updated;
-    var server = s.createServer(6000, function(){
-      var db = s.createServer(6001, function(){
-        pigeons = new Pigeons({
-          server: 'http://localhost:6000',
-          home  : '/',
-        }, function(){
-          this.db = 'http://localhost:6001';
-          this.getAll();
-        });
-      })
-      db.once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [
+    pigeons = new Pigeons({
+      server: 'http://localhost:6000',
+      home  : '/',
+    }, function(){
+      this.db = 'http://localhost:6001';
+      this.getAll();
+    });
+
+    db.once('/_design/Timetable/_view/active', s.createResponse(JSON.stringify({rows: [
         { value: { _id: 'foo', type: 'Timetable', source: 'http://mpk.krakow.pl/timetables/1', valid_from: '21.01.2011' }}
       ]})));
-      db.once('/_design/Timetable/_update/dump/foo?new_doc_since=null', function(req, resp){
-        resp.end();
-        db.close();
-        server.close();
-        updated = true;
-      });
+    db.once('/_design/Timetable/_update/dump/foo?new_doc_since=null', function(req, resp){
+      updated = true;
     });
     server.once('/', s.createResponse(CreateDocument()));
 
@@ -45,18 +54,17 @@ describe('local cache check', function(){
       var finish, callback = jasmine.createSpy();
       var html = "<div class=\"valid_from\">28.01.2011</div>";
       var pigeons = new Pigeons({ server: 'http://localhost:6000', get: { valid_from: '.valid_from' }});
-      spyOn(pigeons, 'put');
 
-      var server = s.createServer(6000, function(){
-        pigeons.existing = {'http://localhost:6000/timetables/2': {valid_from: '28.01.2011'}};
-        pigeons.getTimetable('/timetables/2', callback);
-        finish = true;
-      })
-      server.once('/timetables/2', s.createResponse(CreateDocument(html)))
+      spyOn(pigeons, 'put');
+      server.once('/timetables/2', function(req,resp){
+        finish = true; s.createResponse(CreateDocument(html))(req,resp)
+      });
+
+      pigeons.existing = {'http://localhost:6000/timetables/2': {valid_from: '28.01.2011'}};
+      pigeons.getTimetable('/timetables/2', callback);
 
       waitsFor(function(){ return finish });
       runs(function(){
-        server.close();
         expect(pigeons.put).not.toHaveBeenCalled();
         expect(callback).toHaveBeenCalled();
       });
