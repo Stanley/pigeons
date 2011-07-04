6fe442e70b499e319dab6d022c67ae8a9e364a2d
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-07-04 16:33:44 +0200

    - creating resource's database if does not exist - _design/Timetable active view

--- spec/CouchDBSpec.js
+++ spec/CouchDBSpec.js
@@ -5,44 +5,28 @@ describe('couchdb', function(){
   var request = require('request');
   var database = 'http://localhost:5984/test_pigeons2';
 
-  var db;
+  var saved;
   request({method: 'DELETE', uri: database}, function(){
-    var config = {db: database};
-    var pigeons = new Pigeons(config, function(){
-      pigeons.put({_id: "5318008", foo: "bar"}, "<html>Hello!</html>", function(err, resp, body){
-        db = body;
+    request({method: 'PUT', uri: database}, function(err){
+      new Pigeons({db: database}, function(){
+        this.put({_id: "5318008", foo: "bar"}, "<html>Hello!</html>", function(err, resp, body){
+          saved = !err;
+        });
       });
     });
   });
 
   beforeEach(function(){
-    waitsFor(function(){ return db; }, 'Could not create a database', 200);
+    waitsFor(function(){ return saved; }, 'Could not create a database', 200);
   })
-
-  it('should create database', function(){
-    var response;
-
-    request({uri: database}, function(err, resp, body){
-      response = resp;
-    });
-
-    waitsFor(function(){ return response; });
-
-    runs(function(){
-      expect(response.statusCode).toEqual(200);
-    });
-  });
 
   describe('document', function(){
 
     var doc;
-    waitsFor(function(){ return db; }, 'Could not create a database', 200);
-    runs(function(){
+    beforeEach(function(){
       request({uri: database +'/5318008'}, function(err, resp, body){
         doc = JSON.parse(body);
       });
-    });
-    beforeEach(function(){
       waitsFor(function(){ return doc; }, 'Coould not save a document', 100);
     })
 
